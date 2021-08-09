dc16b1f9db7e7d0543b67336a1e74d5c713c8e4d
Ayman A <61881131+aymanxdev@users.noreply.github.com>
2021-08-09 16:42:20 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 const OPEN_WEATHER_BASE_URL = 'https://api.openweathermap.org/data/2.5'
 export const WEATHER = OPEN_WEATHER_BASE_URL + '/weather'
 export const FORECAST = OPEN_WEATHER_BASE_URL + '/forecast'
-export const APP_ID = 19b7dec4c4e6da26f3d8e5c9c60d15ac
+export const APP_ID = '19b7dec4c4e6da26f3d8e5c9c60d15ac'
